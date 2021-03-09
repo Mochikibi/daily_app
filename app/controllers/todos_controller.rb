77251bc88c8c92904f647fedb8cdeace8e112e61
Mochikibi <masaya85710719@icloud.com>
2021-03-09 15:34:46 +0900

    Todoリスト基礎完了

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,44 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.where(user_id: current_user.id).order("limit_time")
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      redirect_to root_path
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    if @todo.destroy
+      redirect_to root_path
+  end
+ end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:task, :state_id, :limit_time).merge(user_id: current_user.id)
   end
 end
+
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :state
+  validates :state_id, numericality: { other_than: 1 } 
+  belongs_to :user
+
 end
+
